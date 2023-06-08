562b05374ffdd3a5676c8db1349132d662d4fa3d
vincentvgit <127845446+vincentvgit@users.noreply.github.com>
2023-06-08 12:32:17 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -18,3 +18,11 @@ terraform {
    name     = "vincent-TP-jenkins01"
    location = "westeurope"
  }
+provider "azurerm" {
+   subscription_id = a2e466aa-4f86-4545-b5b8-97da7c8febf3
+   client_id = 64843f68-7d94-4762-9d90-91eb3d6fcc68
+   client_secret = 18Z8Q~~4FBbVrjdL9_sYBeqTk.UnZyi7zCpdTdpR
+   tenant_id = 7349d3b2-951f-41be-877e-d8ccd9f3e73c
+   skip_provider_registration = true
+   features {}
+}
